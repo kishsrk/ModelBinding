df038d351c411ea92fd7feac3fbf4e9c89a2c897
kishsr <kishsr@DIN76000835>
2017-12-28 14:33:50 +0530

    Add project files.

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(Employee emp)
+        {
+            return View (emp);
+        }
+        [HttpPost]
+        public ActionResult Redirects(Employee emp)
+        {
+            TempData["Emp"] = emp;
+            return Redirect("/Home/Details");
+        }
+        public ActionResult Details()
+        {
+            Employee emp = TempData["Emp"] as Employee;
+            return View(emp);
+        }
+
+    }
+}
